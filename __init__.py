b9e65676336a2203c79a45d63cd8aeb355e4d07c
Florian Boesch <pyalot@gmail.com>
2017-01-19 16:00:16 +0100

    compressing responses for the pages html and added a sitemap

--- __init__.py
+++ __init__.py
@@ -117,6 +117,7 @@ class Site:
     def __call__(self, request, response, config):
         content = open(os.path.join(path, 'index.html'), 'rb').read()
         response['Content-Type'] = 'text/html; charset=utf-8'
+        response.compress = True
         if request.path not in validPaths:
             response.status = '404 Not Found'
         return content
